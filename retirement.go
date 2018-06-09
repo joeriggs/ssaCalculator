3fedab7e0a434936999222ff11f871c0cad4d4d5
Joe Riggs <joe.riggs.1962@gmail.com>
2018-06-09 19:01:26 -0400

    Converted over to using the ssa/data package to read the user data.
    No more fake data setting inside a test file.  It now uses real data
    that was downloaded from ssa.gov.

--- retirement.go
+++ retirement.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/joeriggs/ssa/data"
 	"github.com/joeriggs/retirement/social_security"
 	"github.com/joeriggs/retirement/wages"
 )
@@ -9,7 +10,6 @@ import (
 func main() {
 	fmt.Printf("Beginning the retirement program.\n")
 
-/*
 	fmt.Printf("1985 MaxEarnings %d.\n", social_security.MaxEarnings(1985))
 	fmt.Printf("1995 MaxEarnings %d.\n", social_security.MaxEarnings(1995))
 	fmt.Printf("2005 MaxEarnings %d.\n", social_security.MaxEarnings(2005))
@@ -34,12 +34,22 @@ func main() {
 	fmt.Printf("2015 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
 	a, b, c, d, e  = social_security.BendPoints(2025)
 	fmt.Printf("2025 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-*/
 
 	var person1Wages wages.WageList = make(wages.WageList)
 
-	wages.WageListAdd(1978,    191, person1Wages)
-	
+	statement := data.New("Your_Social_Security_Statement_Data.xml")
+
+	var numEarningsYears = statement.NumEarningsYears()
+
+	for i := 0; i < numEarningsYears; i++ {
+		var year, ficaEarnings, medicareEarnings int
+		year, ficaEarnings, medicareEarnings = statement.EarningsYear(i)
+		fmt.Printf("          %4v   %9v  %9v\n", year, ficaEarnings, medicareEarnings)
+
+		ficaEarningsFloat := float32(ficaEarnings)
+		wages.WageListAdd(year, ficaEarningsFloat, person1Wages)
+	}
+
 	social_security.Benefit(1962, person1Wages)
 }
 
