050c8bec00c8ee2f100dd35f78d8c9effb5ae7a3
Joe Riggs <joe.riggs.1962@gmail.com>
2018-06-12 17:46:23 -0400

    Changed the name of a function.

--- retirement.go
+++ retirement.go
@@ -10,32 +10,7 @@ import (
 func main() {
 	fmt.Printf("Beginning the retirement program.\n")
 
-	fmt.Printf("1985 MaxEarnings %d.\n", social_security.MaxEarnings(1985))
-	fmt.Printf("1995 MaxEarnings %d.\n", social_security.MaxEarnings(1995))
-	fmt.Printf("2005 MaxEarnings %d.\n", social_security.MaxEarnings(2005))
-	fmt.Printf("2015 MaxEarnings %d.\n", social_security.MaxEarnings(2015))
-	fmt.Printf("2025 MaxEarnings %d.\n", social_security.MaxEarnings(2025))
-	fmt.Printf("====================================\n")
-
-	fmt.Printf("1985 AverageWageIndex %f.\n", social_security.AverageWageIndex(1985))
-	fmt.Printf("1995 AverageWageIndex %f.\n", social_security.AverageWageIndex(1995))
-	fmt.Printf("2005 AverageWageIndex %f.\n", social_security.AverageWageIndex(2005))
-	fmt.Printf("2015 AverageWageIndex %f.\n", social_security.AverageWageIndex(2015))
-	fmt.Printf("2025 AverageWageIndex %f.\n", social_security.AverageWageIndex(2025))
-	fmt.Printf("====================================\n")
-
-	a, b, c, d, e := social_security.BendPoints(1985)
-	fmt.Printf("1985 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-	a, b, c, d, e  = social_security.BendPoints(1995)
-	fmt.Printf("1995 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-	a, b, c, d, e  = social_security.BendPoints(2005)
-	fmt.Printf("2005 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-	a, b, c, d, e  = social_security.BendPoints(2015)
-	fmt.Printf("2015 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-	a, b, c, d, e  = social_security.BendPoints(2025)
-	fmt.Printf("2025 BendPoints %d %d %d %d %d.\n", a, b, c, d, e)
-
-	var person1Wages wages.WageList = make(wages.WageList)
+	var person1Wages wages.List = make(wages.List)
 
 	statement := data.New("Your_Social_Security_Statement_Data.xml")
 
@@ -47,7 +22,7 @@ func main() {
 		fmt.Printf("          %4v   %9v  %9v\n", year, ficaEarnings, medicareEarnings)
 
 		ficaEarningsFloat := float32(ficaEarnings)
-		wages.WageListAdd(year, ficaEarningsFloat, person1Wages)
+		wages.Add(year, ficaEarningsFloat, person1Wages)
 	}
 
 	social_security.Benefit(1962, person1Wages)
